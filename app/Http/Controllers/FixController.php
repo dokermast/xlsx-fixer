32f06536148a072f90472011f946edd3e088e43d
vitche <doker.bucket@gmail.com>
2020-09-27 15:40:14 +0300

    TREN added description

--- app/Http/Controllers/FixController.php
+++ app/Http/Controllers/FixController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class FixController extends Controller
 {
+    /* return start page */
     public function index()
     {
         return view('fix', ['checked' => false]);
